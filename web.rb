dfd37507b4852f9f9a9e384d45cd7deb01e9d399
wahiche <dwahiche@infusion.com>
2013-11-28 15:05:04 -0500

    update to web.rb

--- web.rb
+++ web.rb
@@ -1,5 +1,36 @@
 require 'sinatra'
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'digest/md5'
+require 'mail'
 
 get '/' do
-  'Hello world!'
+  sleeper
 end
+
+get '/mail' do
+	options = { :address              => "smtp.gmail.com",
+			:port                 => 587,
+			:user_name            => 'stuffstosells@gmail.com',
+			:password             => 'azsxdcfv',
+			:authentication       => 'plain',
+			:enable_starttls_auto => true  }
+		
+		Mail.defaults do
+			delivery_method :smtp, options
+		end
+		
+		Mail.new(
+			to: 'dwahiche@infusion.com',
+			from: 'stuffstosells@gmail.com',
+			subject: 'BUY NOW!',
+			body: 'HURRY UP BIIIITTTTCCCHHH!'
+			).deliver!
+end
+def sleeper
+	while true
+	'Still Disabled'
+	sleep 3
+end
+
